e80767532b114acb6f6f489db319ac0767a7d78c
masprog2022 <masprog2022@gmail.com>
2024-02-09 22:42:15 +0100

    feat: add MatchRepository

--- server/src/main/java/com/masprogtechs/backend/repository/MatchRepository.java
+++ server/src/main/java/com/masprogtechs/backend/repository/MatchRepository.java
@@ -1,2 +1,13 @@
-package com.masprogtechs.backend.repository;public class MatchRepository {
+package com.masprogtechs.backend.repository;
+
+import com.masprogtechs.backend.model.Match;
+import com.masprogtechs.backend.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface MatchRepository extends JpaRepository<Match, Long> {
+    @Query("SELECT m FROM match m ORDER BY m.date DESC")
+    List<Match> findAllOrderedById();
 }
