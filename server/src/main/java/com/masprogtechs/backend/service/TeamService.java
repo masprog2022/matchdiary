6c1518c118e485844a6b846e6db4b08ffb8f5469
masprog2022 <masprog2022@gmail.com>
2024-02-09 19:42:27 +0100

    feat: add services getAllTeams and getTeamById

--- server/src/main/java/com/masprogtechs/backend/service/TeamService.java
+++ server/src/main/java/com/masprogtechs/backend/service/TeamService.java
@@ -1,2 +1,28 @@
-package com.masprogtechs.backend.service;public class TeamService {
+package com.masprogtechs.backend.service;
+
+import com.masprogtechs.backend.dto.team.TeamRequestDTO;
+import com.masprogtechs.backend.dto.team.TeamResponseDTO;
+import com.masprogtechs.backend.model.Team;
+import com.masprogtechs.backend.repository.TeamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TeamService {
+
+    private final TeamRepository teamRepository;
+
+    public List<Team> getAllTeams(){
+        return teamRepository.findAllOrderedById();
+    }
+
+    public TeamResponseDTO getTeamById(Long id) {
+        Optional<Team> response = teamRepository.findById(id);
+        return response.map(TeamResponseDTO::new).orElseThrow(() ->new RuntimeException("ID não encontrado!"));
+    }
+
 }
