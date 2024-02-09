2ea6fe771f51cb246b2d4d4440167331aead732f
masprog2022 <masprog2022@gmail.com>
2024-02-10 00:04:55 +0100

    feat: add method specific for using in reports

--- server/src/main/java/com/masprogtechs/backend/repository/MatchRepository.java
+++ server/src/main/java/com/masprogtechs/backend/repository/MatchRepository.java
@@ -8,6 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
+
+    @Query(value = "SELECT CAST(EXTRACT(EPOCH FROM AGE(CURRENT_DATE, MAX(date))) / (60*60*24) AS INTEGER) FROM match", nativeQuery = true)
+    Integer getDaysWithoutWatching();
     @Query("SELECT m FROM match m ORDER BY m.date DESC")
     List<Match> findAllOrderedById();
 }
