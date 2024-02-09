89aeb26f748a4e498b3f5277d6b616b16a231d8a
masprog2022 <masprog2022@gmail.com>
2024-02-09 19:41:49 +0100

    feat: add repository

--- server/src/main/java/com/masprogtechs/backend/repository/TeamRepository.java
+++ server/src/main/java/com/masprogtechs/backend/repository/TeamRepository.java
@@ -1,2 +1,13 @@
-package com.masprogtechs.backend.repository;public class TeamRepository {
+package com.masprogtechs.backend.repository;
+
+import com.masprogtechs.backend.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    @Query("SELECT t FROM team t ORDER BY t.id ASC")
+    List<Team> findAllOrderedById();
 }
