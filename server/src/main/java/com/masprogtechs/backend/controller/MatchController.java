5cc318913e98fdcccd2fa5e1d998a079c32515a9
masprog2022 <masprog2022@gmail.com>
2024-02-09 23:18:06 +0100

    feat: add controller addMatch

--- server/src/main/java/com/masprogtechs/backend/controller/MatchController.java
+++ server/src/main/java/com/masprogtechs/backend/controller/MatchController.java
@@ -1,14 +1,12 @@
 package com.masprogtechs.backend.controller;
 
+import com.masprogtechs.backend.dto.match.MatchRequestDTO;
 import com.masprogtechs.backend.dto.match.MatchResponseDTO;
 import com.masprogtechs.backend.model.Match;
 import com.masprogtechs.backend.service.MatchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,11 @@ public class MatchController {
     @GetMapping("/{id}")
     public ResponseEntity<MatchResponseDTO> getMatchById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(matchService.getMatchById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Match> addMatch(@RequestBody MatchRequestDTO matchRequestDTO) {
+        return ResponseEntity.ok(matchService.registerMatch(matchRequestDTO));
     }
 
 }
