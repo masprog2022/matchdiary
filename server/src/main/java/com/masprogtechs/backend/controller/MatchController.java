e68a5ef413f80f28f69971af4520d802e6c67317
masprog2022 <masprog2022@gmail.com>
2024-02-09 22:54:04 +0100

    feat: controller getMatchById

--- server/src/main/java/com/masprogtechs/backend/controller/MatchController.java
+++ server/src/main/java/com/masprogtechs/backend/controller/MatchController.java
@@ -1,9 +1,12 @@
 package com.masprogtechs.backend.controller;
 
+import com.masprogtechs.backend.dto.match.MatchResponseDTO;
 import com.masprogtechs.backend.model.Match;
 import com.masprogtechs.backend.service.MatchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +22,10 @@ public class MatchController {
     public List<Match> getAllMatches() {
         return matchService.getAllMatches();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MatchResponseDTO> getMatchById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(matchService.getMatchById(id));
+    }
+
 }
