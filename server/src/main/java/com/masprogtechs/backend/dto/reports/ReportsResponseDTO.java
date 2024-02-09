d4cabf6e38e38ccc9bc826dfc10befc85ceed3c8
masprog2022 <masprog2022@gmail.com>
2024-02-10 00:04:10 +0100

    feat: report DTO

--- server/src/main/java/com/masprogtechs/backend/dto/reports/ReportsResponseDTO.java
+++ server/src/main/java/com/masprogtechs/backend/dto/reports/ReportsResponseDTO.java
@@ -1,2 +1,18 @@
-package com.masprogtechs.backend.dto.reports;public class ReportsResponseDTO {
+package com.masprogtechs.backend.dto.reports;
+
+import com.masprogtechs.backend.model.Team;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReportsResponseDTO {
+
+    private Integer matchesQuantity;
+    private Integer winsQuantity;
+    private Integer winPercentage;
+    private Team mostWatchedTeam;
+    private Integer daysWithoutWatching;
+
 }
