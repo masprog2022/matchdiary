d779f7482fddc1ea1fc765ab4c2e0e9446388258
masprog2022 <masprog2022@gmail.com>
2024-02-09 22:42:35 +0100

    feat: add service MatchService

--- server/src/main/java/com/masprogtechs/backend/service/MatchService.java
+++ server/src/main/java/com/masprogtechs/backend/service/MatchService.java
@@ -1,2 +1,18 @@
-package com.masprogtechs.backend.service;public class MatchService {
+package com.masprogtechs.backend.service;
+
+import com.masprogtechs.backend.model.Match;
+import com.masprogtechs.backend.repository.MatchRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MatchService {
+    private final MatchRepository matchRepository;
+
+    public List<Match> getAllMatches() {
+        return matchRepository.findAllOrderedById();
+    }
 }
