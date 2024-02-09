7d5f2059e51120e1164e1198d3e1c88e718eef09
masprog2022 <masprog2022@gmail.com>
2024-02-10 00:05:21 +0100

    feat: add method specific for using in report

--- server/src/main/java/com/masprogtechs/backend/repository/TeamRepository.java
+++ server/src/main/java/com/masprogtechs/backend/repository/TeamRepository.java
@@ -8,6 +8,15 @@ import java.util.List;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
+    @Query("SELECT t " +
+            "FROM team t " +
+            "WHERE t.id = (" +
+            "   SELECT COALESCE(m.teamOne.id, m.teamTwo.id) " +
+            "   FROM match m " +
+            "   GROUP BY COALESCE(m.teamOne.id, m.teamTwo.id) " +
+            "   ORDER BY COUNT(COALESCE(m.teamOne.id, m.teamTwo.id)) DESC" +
+            "   LIMIT 1)")
+    Team findMostWatchedTeam();
     @Query("SELECT t FROM team t ORDER BY t.id ASC")
     List<Team> findAllOrderedById();
 }
