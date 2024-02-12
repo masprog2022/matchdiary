8eeebf4af6140d60f96fe52cf69b91a8453b161e
masprog2022 <masprog2022@gmail.com>
2024-02-12 02:57:46 +0100

    feat: add Title e card in reports

--- web/src/pages/reports/ReportsPage.tsx
+++ web/src/pages/reports/ReportsPage.tsx
@@ -1,9 +1,21 @@
+import Card from "../../components/Card";
+import CardContainer from "../../components/CardContainer";
+import Container from "../../components/Container";
 import { Navbar } from "../../components/Navbar";
+import Title from "../../components/Title";
 
 export default function ReportsPage() {
   return (
     <>
       <Navbar />
+      <Container>
+        <Title title="O resumo do seu diário de torcedor!" />
+        <CardContainer>
+          <Card value="89" description="partidas" />
+          <Card value="43" description="vitórias" />
+          <Card value="48%" description="de aproveitamento" />
+        </CardContainer>
+      </Container>
     </>
   );
 }
