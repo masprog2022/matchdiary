5aa65e090544dbd2e93a7c2e482a498140e87ef8
masprog2022 <masprog2022@gmail.com>
2024-02-19 16:58:23 +0100

    feat: add TeamTable component

---
+++ web/src/components/TeamTable.tsx
@@ -0,0 +1,102 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { IconButton } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { TeamData } from "../interfaces/TeamData";
+import api from "../services/api";
+
+const TableStyled = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  overflow: auto;
+`;
+
+const TheadStyled = styled.thead`
+  background-color: #efefee;
+`;
+
+const ThStyled = styled.th`
+  font-family: "Futura";
+  background-color: #efefee;
+  text-transform: uppercase;
+  border: 1px solid #ddd;
+`;
+
+const TdStyled = styled.td`
+  font-family: "Futura";
+  background-color: #efefee;
+  color: black;
+  text-align: center;
+  text-transform: uppercase;
+  height: 150px;
+  font-size: 25px;
+  border: 1px solid #ddd;
+`;
+
+interface TeamTableProps {
+  teams: TeamData[];
+}
+
+export default function TeamTable(props: TeamTableProps) {
+  const navigate = useNavigate();
+
+  const handleEditTeam = async (e: React.FormEvent, teamId: number) => {
+    e.preventDefault();
+
+    navigate(`/team/edit/${teamId}`);
+  };
+
+  const handleDeleteTeam = async (e: React.FormEvent, teamId: number) => {
+    e.preventDefault();
+
+    try {
+      await api.deleteTeam(teamId);
+      window.location.reload();
+    } catch (error) {
+      console.log("Erro ao deletar time", error);
+    }
+  };
+
+  return (
+    <TableStyled>
+      <TheadStyled>
+        <tr>
+          <ThStyled>Id</ThStyled>
+          <ThStyled></ThStyled>
+          <ThStyled>Time</ThStyled>
+          <ThStyled>Estado</ThStyled>
+          <ThStyled>Ações</ThStyled>
+        </tr>
+      </TheadStyled>
+      <tbody>
+        {props.teams.map((team: TeamData) => (
+          <tr key={team.id}>
+            <TdStyled>{team.id}</TdStyled>
+            <TdStyled>
+              <img
+                src={team.photoUrl}
+                alt={team.name}
+                style={{ width: "120px", padding: "10px" }}
+              />
+            </TdStyled>
+            <TdStyled>{team.name}</TdStyled>
+            <TdStyled>{team.state}</TdStyled>
+            <TdStyled>
+              <IconButton
+                onClick={(e: React.FormEvent) => handleEditTeam(e, team.id!)}
+              >
+                <EditIcon />
+              </IconButton>
+              <IconButton
+                onClick={(e: React.FormEvent) => handleDeleteTeam(e, team.id!)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </TdStyled>
+          </tr>
+        ))}
+      </tbody>
+    </TableStyled>
+  );
+}
--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,8 +1,11 @@
 import axios, { AxiosResponse } from "axios";
 import { ReportsData } from "../interfaces/ReportsData";
+import { TeamData } from "../interfaces/TeamData";
 
 interface Api {
   getReportsData: () => Promise<AxiosResponse<ReportsData>>;
+  deleteTeam: (id: number) => Promise<AxiosResponse<any>>;
+  getTeamData: () => Promise<AxiosResponse<TeamData[]>>;
 }
 
 const instance = axios.create({
@@ -14,6 +17,8 @@ const instance = axios.create({
 
 const api: Api = {
   getReportsData: () => instance.get("/reports"),
+  getTeamData: () => instance.get("/team/all"),
+  deleteTeam: (id: number) => instance.delete(`/team/${id}`),
 };
 
 export default api;
